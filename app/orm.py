ac9923622f8be5f6b8aefb8aebabc6308ea42060
xp <haopingl.xu@blockheaders.com>
2020-06-08 18:44:48 +0800

    add: booleanField

--- app/orm.py
+++ app/orm.py
@@ -1,3 +1,6 @@
+
+
+
 async def create_pool(loop, **kw):
     loogging.info('create database connection pool........')
     global __pool
@@ -158,3 +161,24 @@ class StringField(Field):
 
     def __init__(self, name=None, primary_key=False, default=None, ddl='varchar(100)'):
         super().__init__(name, ddl, primary_key, default)
+
+
+class BooleanField(Field):
+
+    def __init__(self, name=None, default=False):
+        super().__init__(name, 'boolean', False, default)
+
+class IntegerField(Field):
+
+    def __init__(self, name=None, primary_key=False, default=0):
+        super().__init__(name, 'bigint', primary_key, default)
+
+class FloatField(Field):
+
+    def __init__(self, name=None, primary_key=False, default=0.0):
+        super().__init__(name, 'real', primary_key, default)
+
+class TextField(Field):
+
+    def __init__(self, name=None, default=None):
+        super().__init__(name, 'text', False, default)
