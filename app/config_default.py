a84213c4eb0e9ecb8fc2f133e389557f906dd524
xp <haopingl.xu@blockheaders.com>
2020-06-10 18:47:32 +0800

    add: config.py

---
+++ app/config_default.py
@@ -0,0 +1,15 @@
+#config_default.py
+
+configs = {
+    'db': {
+        'host': '127.0.0.1',
+        'port': 3306,
+        'user': 'root',
+        'password': '123',
+        'database': 'awesome'
+    },
+    'session': {
+        'secret': 'AwEsOmE'
+    }
+   
+}
